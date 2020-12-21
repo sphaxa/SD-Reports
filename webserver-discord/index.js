111c17744ee354a6d93ed5220ae1a06366a03473
sphhax <zskaistis2@gmail.com>
2020-12-21 10:45:27 -0600

    updated modules and added logging schema

--- webserver-discord/index.js
+++ webserver-discord/index.js
@@ -1,28 +1,15 @@
-require('dotenv').config()
+require('dotenv').config();
+require('console-info');
+require('console-warn');
+require('console-error');
+const mod = require('./modules');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  run_check = checkServer();
-  if (!run_check[0]) sendError(run_check[1]);
-  
+  run_check = mod.helper.checkServer();
+  if (!run_check[0]) mod.helper.sendOutput('error', run_check[1]);
+
 });
-
-/*
-    HELPER FUNCTIONS
-*/
-
-//Check server to make sure we have the right permissions, as well as validate the channel and role id.
-function checkServer()
-{
-    if (!process.env.CHANNEL_ID || !process.env.PING_ROLE_ID || !process.env.SECURITY_KEY)
-        return [false, 'Plugin is misconfigured\nPlease fill out .env'];
-
-}
-
-function sendError(error)
-{
-    console.log("ERROR: " + error);
-}
 
 client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ webserver-discord/modules/index.js
@@ -0,0 +1,1 @@
+exports.helper = require("./helperFunc");
